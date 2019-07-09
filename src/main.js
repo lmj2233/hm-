4ccb2d4724d274e5fc0c894e1d63675e611a2e5d
lml6666 <1483557675@qq.com>
2019-07-09 19:21:26 +0800

    下载ElementUI组件，并进行相关配置，注册为全局组件

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 导入 ElementUI 组件库和样式
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 在全局注册 ElementUI 组件
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
